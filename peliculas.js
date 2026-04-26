47c0c694cc8f52200478bbd77f71273023f2b37d
gaeloy <240300978@ucaribe.edu.mx>
2026-04-26 01:15:02 -0500

    nuevo GET/directores

---
+++ peliculas.js
@@ -0,0 +1,36 @@
+console.log("Archivo correcto ;)");
+
+const express = require('express');
+const app = express();
+const port = process.env.port || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Lista de películas' });
+});
+
+app.get('/directores', (req, res) => {
+    res.json({ message: 'Lista de directores' });
+});
+
+app.post('/peliculas', (req, res) => {
+    const nuevaPelicula = req.body;
+    res.json({ message: 'Película creada', pelicula: nuevaPelicula });
+});
+
+app.put('/peliculas/:id', (req, res) => {
+    const id = req.params.id;
+    const peliculaActualizacion = req.body;
+    res.json({ message: `Película con ID ${id} actualizada`, peliculaActualizada });
+});
+
+app.delete('/peliculas/:id', (req, res) => {
+    const id = req.params.id;
+    res.json({ message: `Película con ID ${id} eliminada` });
+});
+
+// Servidor
+app.listen(port, () => {
+    console.log(`Servidor en puerto: ${port}`);
+});
